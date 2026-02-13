84399fadb1f248e77c5be673ba636d32e7bb07a9
Guilherme Mendes <guilhermemendeds@gmail.com>
2026-02-13 19:43:42 -0300

    feat: clean architecture

---
+++ src/infra/http/controllers/create-account.controller.ts
@@ -0,0 +1,41 @@
+import { Body, ConflictException, Controller, HttpCode, Post, UsePipes } from "@nestjs/common";
+import { hash } from "bcryptjs";
+import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
+import { PrismaService } from "@/infra/prisma/prisma.service";
+import { z } from "zod";
+
+const createAccountBodySchema = z.object({
+    name: z.string(),
+    email: z.email(),
+    password: z.string()
+});
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
+
+@Controller('/accounts')
+export class CreateAccountController {
+    constructor(private prisma: PrismaService) {}
+
+    @Post()
+    @HttpCode(201)
+    @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+    async handle(@Body() body: CreateAccountBodySchema) {
+        const { name, email, password } = body;
+
+        const userWithSameEmail = await this.prisma.user.findUnique({
+            where: {
+                email
+            }
+        });
+
+        if (userWithSameEmail) {
+            throw new ConflictException('User with same e-mail address already exists.');
+        }
+
+        await this.prisma.user.create({ data: {
+            name,
+            email,
+            password: await hash(password, 8)
+        }});
+    }
+}
