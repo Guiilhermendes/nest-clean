bf2dd7f7245dc52135d66bd1a27c2d465cbe6da6
Guilherme Mendes <guilhermemendeds@gmail.com>
2026-02-27 19:03:47 -0300

    feat/fix: creating edit question controller and your test / switching interface to abstract class

---
+++ src/infra/http/controllers/edit-question.controller.e2e-spec.ts
@@ -0,0 +1,62 @@
+import { AppModule } from "@/infra/app.module";
+import { DatabaseModule } from "@/infra/database/database.module";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { INestApplication } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Test } from "@nestjs/testing";
+import request from "supertest";
+import { QuestionFactory } from "test/factories/make-question";
+import { StudentFactory } from "test/factories/make-students";
+
+describe('Edit question (E2E)', () => {
+    let app: INestApplication;
+    let prisma: PrismaService;
+    let studentFactory: StudentFactory;
+    let questionFactory: QuestionFactory;
+    let jwt: JwtService;
+
+    beforeAll(async () => {
+        const moduleRef = await Test.createTestingModule({
+            imports: [AppModule, DatabaseModule],
+            providers: [StudentFactory, QuestionFactory]
+        }).compile();
+
+        app = moduleRef.createNestApplication();
+        prisma = moduleRef.get(PrismaService);
+        studentFactory = moduleRef.get(StudentFactory);
+        jwt = moduleRef.get(JwtService);
+        questionFactory = moduleRef.get(QuestionFactory);
+
+        await app.init();
+    });
+
+    test('[PUT] /questions/:id', async () => {
+        const title = 'New title';
+
+        const user = await studentFactory.makePrismaStudent();
+        const userId = user.id.toString();
+        const accessToken = jwt.sign({ sub: userId });
+
+        const question = await questionFactory.makePrismaStudent({
+            authorId: user.id
+        })
+
+        const response = await request(app.getHttpServer())
+            .put(`/questions/${question.id.toString()}`)
+            .set('Authorization', (`Bearer ${accessToken}`))
+            .send({
+                title,
+                content: 'New content'
+            });
+
+        expect(response.statusCode).toEqual(204)
+
+        const questionOnDatabase = await prisma.question.findFirst({
+            where: {
+                title
+            }
+        });
+
+        expect(questionOnDatabase).toBeTruthy();
+    });
+});
